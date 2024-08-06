4b92dc165b55b77abc79bebbcf72ccf0970ae679
seo-jae-eun <wodms5522@naver.com>
2024-08-06 21:38:11 +0900

    [0909] 코드 수정

--- vue/project/src/pages/Common/LoginPage.vue
+++ vue/project/src/pages/Common/LoginPage.vue
@@ -74,7 +74,7 @@
           <div id="snsLogin" class="snsLogin" v-if="activeOption === 'user'" style="display: flex;">
             <ul>
               <li id="kakao_li">
-                <a href="/api/login/oauth2/code/kakao" class="kakao" id="openid_kakao">
+                <a href="/api/oauth2/authorization/kakao" class="kakao" id="openid_kakao">
                   <span>카카오톡</span>
                 </a>
               </li>
